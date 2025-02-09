54f22e7d371070f328017264e536545dc86bf49f
ram <rrsubramoniam98@gmail.com>
2025-02-10 01:00:48 +0530

    Initial commit

---
+++ test/ram_personal_project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RamPersonalProject from '../lib/ram_personal_project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ram_personal_project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RamPersonalProject.RamPersonalProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
